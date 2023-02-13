b7145fa923ea34babe2001d3ae074ba4ebf92fa5
senrajkumar1998 <senrajkumar1998@gmail.com>
2023-02-13 11:22:55 +0530

    my pizza

--- src/main/java/com/driver/Pizza.java
+++ src/main/java/com/driver/Pizza.java
@@ -10,25 +10,78 @@ public class Pizza {
     boolean isBillCreated;
     boolean isTakeAway;
 
+    public void setPrice(int price) {
+        this.price = price;
+    }
 
+    public Boolean getVeg() {
+        return isVeg;
+    }
 
-    public Pizza(Boolean isVeg){
+    public void setVeg(Boolean veg) {
+        isVeg = veg;
+    }
+
+    public void setBill(String bill) {
+        this.bill = bill;
+    }
+
+    public int getToppings() {
+        return toppings;
+    }
+
+    public void setToppings(int toppings) {
+        this.toppings = toppings;
+    }
+
+    public boolean isExtraCheeseAdded() {
+        return isExtraCheeseAdded;
+    }
+
+    public void setExtraCheeseAdded(boolean extraCheeseAdded) {
+        isExtraCheeseAdded = extraCheeseAdded;
+    }
+
+    public boolean isExtraToppingAdded() {
+        return isExtraToppingAdded;
+    }
+
+    public void setExtraToppingAdded(boolean extraToppingAdded) {
+        isExtraToppingAdded = extraToppingAdded;
+    }
+
+    public boolean isBillCreated() {
+        return isBillCreated;
+    }
+
+    public void setBillCreated(boolean billCreated) {
+        isBillCreated = billCreated;
+    }
+
+    public boolean isTakeAway() {
+        return isTakeAway;
+    }
+
+    public void setTakeAway(boolean takeAway) {
+        isTakeAway = takeAway;
+    }
+
+    public Pizza(Boolean isVeg) {
         this.isVeg = isVeg;
         // your code goes here
-        if(isVeg){
-            this.price=300;
-            this.toppings=70;
-        }
-        else {
-            this.price=400;
-            this.toppings=120;
+        if (isVeg) {
+            this.price = 300;
+            this.toppings = 70;
+        } else {
+            this.price = 400;
+            this.toppings = 120;
         }
         /*this.isExtraCheeseAdded=false;
         this.isExtraToppingAdded=false;
         this.isBillCreated=false;
         this.isTakeAway=false;*/
 
-        this.bill="Base Price Of The Pizza: "+this.price+"\n";
+        this.bill = "Base Price Of The Pizza: "+this.price+"\n";
     }
 
     public int getPrice(){
@@ -71,7 +124,7 @@ public class Pizza {
             if (isTakeAway) {
                 this.bill += "Paperbag Added: 20" + "\n";
             }
-            this.bill += "Total Price: " +this.price;
+            this.bill += "Total Price: "+this.price;
             this.isBillCreated=true;
             return this.bill;
         }
